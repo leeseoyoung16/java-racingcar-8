40b17b24f8017b5aaa1b3cd43eb96853b47bc6ab
seo <tjdud11160@gmail.com>
2025-10-25 15:17:16 +0900

    feat: 경주 결과 및 우승자 출력 기능 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,35 @@
+package racingcar.view;
+
+import racingcar.domain.RaceCar;
+
+import java.util.Set;
+
+public class OutputView {
+    public void displayRoundResult(Set<RaceCar> raceCars) {
+        for(RaceCar car : raceCars) {
+            displayCarRoundResult(car);
+        }
+        displayEmptyLine();
+    }
+    private void displayCarRoundResult(RaceCar car) {
+        System.out.println(car.getName() + " : " + formatCarLocation(car.getLocation()));
+    }
+
+    public void displayResultHeader() {
+        System.out.println("실행 결과");
+    }
+
+    public void displayWinner(Set<String> winners) {
+        System.out.print("최종 우승자 : ");
+        String result = String.join(", ", winners);
+        System.out.println(result);
+    }
+
+    private void displayEmptyLine() {
+        System.out.println();
+    }
+
+    private String formatCarLocation(int location) {
+        return "-".repeat(location);
+    }
+}
