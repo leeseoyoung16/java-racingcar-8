c55cb1766ced37aab2af04467e0f95d8dd7d91f8
seo <tjdud11160@gmail.com>
2025-10-25 15:12:28 +0900

    test: RaceCarGroup 단위 테스트 추가 (자동차 그룹 생성 및 예외 검증)

---
+++ src/test/java/racingcar/model/RaceCarGroupTest.java
@@ -0,0 +1,38 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.RaceCar;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RaceCarGroupTest {
+    private RaceCarGroup raceCarGroup;
+
+    @Test
+    void 정상_경주_그룹_테스트() {
+        //given
+        String input = "pobi,woni,xeo";
+        //when
+        raceCarGroup = new RaceCarGroup(input);
+        //then
+        Set<RaceCar> cars = raceCarGroup.getCars();
+        assertEquals(cars.size(), 3);
+
+        assertTrue(cars.stream().anyMatch(car -> car.getName().equals("pobi")));
+        assertTrue(cars.stream().anyMatch(car -> car.getName().equals("woni")));
+        assertTrue(cars.stream().anyMatch(car -> car.getName().equals("xeo")));
+    }
+
+    @Test
+    void 자동차가_한대이면_예외발생() {
+        //given
+        String input = "pobi";
+
+        //when + then
+        assertThrows(IllegalArgumentException.class, () -> new RaceCarGroup(input));
+    }
+
+}
